ffc8728552a5fab767f87eeea2bf93c318b6dc3c
Simplyandyvilla <Simplyandyvilla@gmail.com>
2023-07-15 08:59:39 -0700

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
 {
-	//declaring my string
+	/*declaring my string*/
     char Mystr[] = "Programming is like building a multilingual puzzle\n";
     
-    puts(Mystr); //printing my declared string
+    puts(Mystr); /*printing my declared string*/
     
     return 0;
 }
