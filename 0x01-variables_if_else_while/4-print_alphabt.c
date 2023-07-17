e95cfde45c641ccf13af74ddce51bee9fb10b98b
Simplyandyvilla <Simplyandyvilla@gmail.com>
2023-07-17 14:45:10 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *  main - prints the alphabet minus e and q,
+ *  followed by a new line
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int alp;
+
+	for (alp = 'a'; alp <= 'z'; alp++)
+	{
+		if (alp != 'e' && alp != 'q')
+		putchar(alp);
+	}
+	putchar('\n');
+	return (0);
+}
