8c6585a72c97fe899a1c76e2647e4b0969a10fc2
Simplyandyvilla <Simplyandyvilla@gmail.com>
2023-07-17 15:17:00 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet in lowercase, and then in uppercase,
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		printf("%d\n", i);
+	}
+	printf("\n");
+	return (0);
+}
